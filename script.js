ad7819b6470892b060b2d9ca530e49083123933d
Hammad Ullah <62196971+mhammadullah@users.noreply.github.com>
2023-02-25 02:29:51 +0500

    Add files via upload

---
+++ script.js
@@ -0,0 +1,30 @@
+let MenuBtn= document.getElementById('MenuBtn')
+MenuBtn.addEventListener('click',function(e){
+    document.querySelector('body').classList.toggle('mobile-nav-active');
+    this.classList.toggle('fa-xmark')
+})
+// typing effect
+let typed =new Typed('#auto-input', {
+    strings:['Front-End Developer!','UI Designer!', 'Youtuber!','Engineer'],
+    typedSpeed:100,
+    backSpeed:100,
+    backDelay:2000,
+    loop:true,
+})
+// activate link state on scroll
+let navLinks=document.querySelectorAll('nav ul li a')
+let sections=document.querySelectorAll('section')
+
+window.addEventListener('scroll',function(){
+    const scrollPos=window.scrollY +20
+    sections.forEach(section =>{
+        if(scrollPos > section.offsetTop && scrollPos < (section.offsetTop + section.offsetHeight)){
+            navLinks.forEach(link =>{
+                link.classList.remove('active');
+                if(section.getAttribute('id') === link.getAttribute('href').substring(1)){
+                    link.classList.add('active')
+                }
+            });
+        }
+    });
+});
